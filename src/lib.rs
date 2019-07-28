1fa0c7af855af5b2cf4f6980dd9b1c4099eb2aef
sonro <sonro@gmx.com>
2019-07-28 21:09:11 +0000

    Setup defaults

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,30 @@ pub struct Editor {
     buffer: String
 }
 
+impl Default for Editor {
+    fn default() -> Self {
+        Editor {
+            newline_count_trigger: 2,
+            new_text: String::From("-------\n"),
+            current_count: 0,
+            buffer: String::new(),
+        }
+    }
+}
+
 pub struct LinurgyBuilder {
     input:  Input,
     output: Output,
     editor: Editor,
+}
+
+impl Default for LinurgyBuilder {
+    fn default() -> Self {
+        LinurgyBuilder {
+            input: Input::StdIn,
+            output: Output::StdOut,
+            editor: Editor::default(),
+        }
+    }
 }
 
