7edb94dfe157c5be83285b7fa5b4a964e7533242
Christopher Morton <sonro@gmx.com>
2022-01-26 12:53:46 +0000

    Create BasicBuilder
    Create a BasicEditor builder for a more intuitive API.

--- src/builder/basic.rs
+++ src/builder/basic.rs
@@ -1,24 +1,122 @@
 #![allow(dead_code, clippy::derivable_impls)]
-use crate::editor::{BasicEditor, NewlineType};
+use super::build_macro::impl_common_builder;
+use crate::{
+    editor::{BasicEditor, NewlineType},
+    EditType,
+};
 
-#[derive(Debug)]
-pub struct EditorBuilder {}
+#[derive(Debug, PartialEq)]
+pub struct EditorBuilder<'a> {
+    replace: String,
+    trigger: u8,
+    newline: NewlineType,
+    text: &'a str,
+    edit_type: EditType,
+    dirty: bool,
+}
 
-impl Default for EditorBuilder {
+impl<'a> Default for EditorBuilder<'a> {
     fn default() -> Self {
-        Self {}
+        Self {
+            replace: String::new(),
+            trigger: 0,
+            newline: NewlineType::Lf,
+            text: "",
+            edit_type: EditType::Replace,
+            dirty: false,
+        }
     }
 }
 
-impl EditorBuilder {
+impl<'a> EditorBuilder<'a> {
     pub fn new() -> Self {
         Self::default()
     }
 
-    pub fn build(&self) -> BasicEditor {
-        let replace = "";
-        let trigger = 0;
-        let newline = NewlineType::Lf;
-        BasicEditor::new(replace, trigger, newline)
+    pub fn build(&mut self) -> BasicEditor {
+        if self.dirty {
+            self.prepare();
+        }
+        BasicEditor::new(&self.replace, self.trigger, self.newline)
+    }
+
+    pub fn build_prepared(&self) -> BasicEditor {
+        BasicEditor::new(&self.replace, self.trigger, self.newline)
+    }
+
+    impl_common_builder!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_build_same_as_editor_default() {
+        let mut builder = EditorBuilder::default();
+        assert_eq!(BasicEditor::default(), builder.build());
+    }
+
+    #[test]
+    fn new_just_creates_default() {
+        let default = EditorBuilder::default();
+        let new = EditorBuilder::new();
+        assert_eq!(default, new);
+    }
+
+    #[test]
+    fn set_newline_trigger() {
+        let mut builder = EditorBuilder::new();
+        builder.newline_trigger(2);
+        let expected = BasicEditor::new("", 2, NewlineType::Lf);
+        assert_eq!(expected, builder.build());
+    }
+
+    #[test]
+    fn default_edit_type_replace() {
+        let default = EditorBuilder::default();
+        let mut explicit_replace = EditorBuilder::new();
+        explicit_replace.replace();
+        assert_eq!(default.edit_type, explicit_replace.edit_type);
+    }
+
+    #[test]
+    fn replace_with_dashes() {
+        let mut builder = EditorBuilder::new();
+        builder.text("--");
+        let expected = BasicEditor::new("--", 0, NewlineType::Lf);
+        assert_eq!(expected, builder.build());
+    }
+
+    #[test]
+    fn append_with_dashes() {
+        let mut builder = EditorBuilder::new();
+        builder.text("--").append();
+        let expected = BasicEditor::new("\n--", 0, NewlineType::Lf);
+        assert_eq!(expected, builder.build());
+    }
+
+    #[test]
+    fn insert_with_dashes() {
+        let mut builder = EditorBuilder::new();
+        builder.text("--").insert();
+        let expected = BasicEditor::new("--\n", 0, NewlineType::Lf);
+        assert_eq!(expected, builder.build());
+    }
+
+    #[test]
+    fn default_newline_type_lf() {
+        let default = EditorBuilder::default();
+        let mut explicit_lf = EditorBuilder::new();
+        explicit_lf.newline_lf();
+        assert_eq!(default.newline, explicit_lf.newline);
+    }
+
+    #[test]
+    fn crlf_newline_append_with_dashes() {
+        let mut builder = EditorBuilder::new();
+        builder.newline_crlf().text("--").append();
+        let expected = BasicEditor::new("\r\n--", 0, NewlineType::Crlf);
+        assert_eq!(expected, builder.build());
     }
 }
--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -1,2 +1,74 @@
 mod basic;
 mod buffer;
+
+pub use basic::EditorBuilder as BasicBuilder;
+
+mod build_macro {
+    macro_rules! impl_common_builder {
+        () => {
+            pub fn prepare(&mut self) {
+                // avoid re-allocation
+                self.replace.clear();
+
+                match self.edit_type {
+                    EditType::Append => {
+                        self.replace.push_str(self.newline.as_str());
+                        self.replace.push_str(self.text);
+                    }
+                    EditType::Insert => {
+                        self.replace.push_str(self.text);
+                        self.replace.push_str(self.newline.as_str());
+                    }
+                    EditType::Replace => self.replace.push_str(self.text),
+                }
+
+                // prevent re-allocation
+                self.dirty = false;
+            }
+
+            pub fn newline_trigger(&mut self, trigger: u8) -> &mut Self {
+                self.dirty = true;
+                self.trigger = trigger;
+                self
+            }
+
+            pub fn text(&mut self, text: &'a str) -> &mut Self {
+                self.dirty = true;
+                self.text = text;
+                self
+            }
+
+            pub fn append(&mut self) -> &mut Self {
+                self.dirty = true;
+                self.edit_type = EditType::Append;
+                self
+            }
+
+            pub fn insert(&mut self) -> &mut Self {
+                self.dirty = true;
+                self.edit_type = EditType::Insert;
+                self
+            }
+
+            pub fn replace(&mut self) -> &mut Self {
+                self.dirty = true;
+                self.edit_type = EditType::Replace;
+                self
+            }
+
+            pub fn newline_lf(&mut self) -> &mut Self {
+                self.dirty = true;
+                self.newline = NewlineType::Lf;
+                self
+            }
+
+            pub fn newline_crlf(&mut self) -> &mut Self {
+                self.dirty = true;
+                self.newline = NewlineType::Crlf;
+                self
+            }
+        };
+    }
+
+    pub(super) use impl_common_builder;
+}
